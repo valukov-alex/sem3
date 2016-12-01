b6b2068507484505384698d54273effcaf9e900a
Саня <valukov.alex@gmail.com>
2016-12-02 02:01:05 +0300

    rewrite

--- find_file.c
+++ find_file.c
@@ -7,51 +7,50 @@
 #include <stdlib.h>
 #include <string.h>
 
-int n = 9;
-char file_name[10] = "stack.c\0";
+char file_name[] = "stack.c\0";
 
 
-void DFS(char* path, int debt, char* filename)
+void DFS(char* path, int debt, char* filename, int *isFound)
 {
-	if(0 <= debt)
+	if(debt < 0)
+		return;
+	DIR* d;
+	struct dirent *dir;
+	struct stat *buf;
+
+	dir = malloc(sizeof(struct dirent));
+	buf = malloc(sizeof(struct stat));
+	d = opendir(path);
+	while((dir = readdir(d)) != NULL)
 	{
-		DIR* d;
-		struct dirent *dir;
-		struct stat *buf;
-	
-		dir = malloc(sizeof(struct dirent));
-		buf = malloc(sizeof(struct stat));
-		d = opendir(path);
-		while((dir = readdir(d)) != NULL)
+		printf("%s %d %ld %d\n", dir->d_name, dir->d_type, buf->st_size, buf->st_mode);
+		if(!strcmp(filename, dir->d_name))
 		{
-			stat(dir->d_name, buf);
-			printf("%s %d %ld %d\n", dir->d_name, dir->d_type, buf->st_size, buf->st_mode);
-			if(!strcmp(filename, dir->d_name))
-			{
-				printf("НАШЛИ В ПАПКЕ %s\n",path);
-				exit(-1);
-			}
-			if(dir->d_type == 4 && strcmp(dir->d_name,"..") && strcmp(dir->d_name,".")) 
-			{
-				printf("\nвошли в папку %s\n\n", dir->d_name);
-				char lol[100];
-				strcpy(lol, path);
-				strcat(lol, "/");
-				strcat(lol, dir->d_name);
-				DFS(lol, debt-1, file_name);
-				printf("\nвышли из папки %s\n\n", dir->d_name);
-			}
+			printf("НАШЛИ В ПАПКЕ %s\n",path);
+			*isFound = 1;
+			return;
+		}
+		if(dir->d_type == 4 && strcmp(dir->d_name,"..") && strcmp(dir->d_name,".")) 
+		{
+			printf("\nвошли в папку %s\n\n", dir->d_name);
+			char way[100];
+			strcpy(way, path);
+			strcat(way, "/");
+			strcat(way, dir->d_name);
+			DFS(way, debt - 1, file_name, isFound);
+			printf("\nвышли из папки %s\n\n", dir->d_name);
 		}
 	}
+
 	return;
 }
 
 int main(int argc, char **argv)
 {
 	int debt = 2;
-	DFS(".", debt, file_name);
+	int isFound = 0;
+	DFS(".", debt, file_name, &isFound);
+	printf("%d\n", isFound);
 	return 0;
 }
-
-
 
